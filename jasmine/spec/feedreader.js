1851501e9efa9246c4bda8507eaeeeb38b7abc8e
Jim Pyle <jimpyle@gmail.com>
2017-12-27 16:03:26 -0600

    added entry class to check for initial entries and removed todo comments

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -38,7 +38,7 @@ $(function() {
             }
         });
     });
-        /* TODO: Write a test that loops through each feed
+        /* Wrote a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -51,7 +51,7 @@ $(function() {
             };
         })
     });
-        /* TODO: Write a test that loops through each feed
+        /* Wrote a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -60,7 +60,7 @@ $(function() {
         let $hamburger = $body.find('.menu-icon-link');
         /* TODO: Write a new test suite named "The menu" */
 
-            /* TODO: Write a test that ensures the menu element is
+            /* Wrote a test that ensures the menu element is
              * hidden by default. You'll have to analyze the HTML and
              * the CSS to determine how we're performing the
              * hiding/showing of the menu element.
@@ -70,7 +70,7 @@ $(function() {
 
         //end of hidden
         });
-        /* TODO: Write a test that ensures the menu changes
+        /* Wrote a test that ensures the menu changes
          * visibility when the menu icon is clicked. This test
          * should have two expectations: does the menu display when
          * clicked and does it hide when clicked again.
@@ -92,7 +92,7 @@ $(function() {
     //declare variables
     describe('Initial Entries', function() {
           var container = $('.feed');
-          var entryLink = $('.entry-link');
+          var entryLink = $('.feed .entry');
           //runs loadFeed to get data
           beforeEach(function(done) {
               loadFeed(0, done);
@@ -109,9 +109,9 @@ $(function() {
         });
 
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* Wrote a new test suite named "Initial Entries" */
 
-        /* TODO: Write a test that ensures when the loadFeed
+        /* Wrote a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
@@ -138,9 +138,9 @@ $(function() {
              });
          });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    /* Wrote a new test suite named "New Feed Selection" */
 
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* Wrote a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
